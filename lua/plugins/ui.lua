aca6e695221ef80e9345466cb811c041536e3476
joreh_ih <pipdreamer@foxmail.com>
2024-11-21 17:49:52 +0800

    1. 将star_dashbord.lua合并入ui.lua, 并再ui.lua去除neo-tree的text; 2. 添加stickyScroll的行数

---
+++ lua/plugins/ui.lua
@@ -0,0 +1,147 @@
+-- if true then return {} end
+return {
+  {
+    { "MunifTanjim/nui.nvim", lazy = true },
+
+    {
+      "nvimdev/dashboard-nvim",
+      lazy = false, -- As https://github.com/nvimdev/dashboard-nvim/pull/450, dashboard-nvim shouldn't be lazy-loaded to properly handle stdin.
+      opts = function()
+        --   local logo = [[
+        --               ▀████▀▄▄              ▄█
+        --                 █▀    ▀▀▄▄▄▄▄    ▄▄▀▀█
+        --         ▄        █          ▀▀▀▀▄  ▄▀
+        --        ▄▀ ▀▄      ▀▄              ▀▄▀
+        --       ▄▀    █     █▀   ▄█▀▄      ▄█
+        --       ▀▄     ▀▄  █     ▀██▀     ██▄█
+        --        ▀▄    ▄▀ █   ▄██▄   ▄  ▄  ▀▀ █
+        --         █  ▄▀  █    ▀██▀    ▀▀ ▀▀  ▄▀
+        --        █   █  █      ▄▄           ▄▀
+        --       ]]
+        --     local logo = [[
+        --     ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠿⠋⠀⢀⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠈⠉⠉⠙⠛⠛⠻⢿⣿⡿⠟⠁⠀⣀⣴⣿⣿⣿⣿⣿⠟
+        --     ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠃⠀⠀⢀⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⠛⣉⣡⠀⣠⣴⣶⣶⣦⠄⣀⡀⠀⠀⠀⣠⣾⣿⣿⣿⣿⣿⡿⢃⣾
+        --     ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡏⠀⣾⣤⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠏⣠⣾⡟⢡⣾⣿⣿⣿⡿⢋⣴⣿⡿⢀⣴⣾⣿⣿⣿⣿⣿⣿⣿⢡⣾⣿
+        --     ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⠃⠀⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠃⣼⣿⡟⣰⣿⣿⣿⣿⠏⣰⣿⣿⠟⣠⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⢚⣛⢿
+        --     ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠏⠀⣠⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣟⠸⣿⠟⢰⣿⣿⣿⣿⠃⣾⣿⣿⠏⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⢋⣾
+        --     ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠻⠻⠃⠀⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡄⢉⣴⣿⣿⣿⣿⡇⠘⣿⣿⠋⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⡘⣿
+        --     ⣿⣿⣿⣿⣿⣿⣿⣿⡿⠿⠿⣿⣿⣿⣿⠁⢀⣀⠀⢀⣾⣿⣿⣿⣿⣿⣿⠟⠉⠉⠉⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣦⣤⣤⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣌
+        --     ⣿⣿⣿⣿⣿⣿⡿⠁⣀⣤⡀⠀⠈⠻⢿⠀⣼⣿⣷⣿⣿⣿⣿⣿⣿⡿⠁⠀⠀⠀⠀⠘⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+        --     ⣿⣿⣿⠟⠛⠙⠃⠀⣿⣿⣿⠀⠀⠀⠀⠀⠙⠿⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀⣾⣿⣿⡿⠿⠿⠿⠿⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠛⠁⠀⠀⠀⠈⠻⣿⣿⣿⣿⣿⣿⣿
+        --     ⣿⠟⠁⢀⣴⣶⣶⣾⣿⣿⣿⣿⣶⡐⢦⣄⠀⠀⠈⠛⢿⣿⣿⣿⣿⡀⠀⠀⠀⠀⢀⣼⡿⢛⣩⣴⣶⣶⣶⣶⣶⣶⣭⣙⠻⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠘⣿⣿⣿⣿⣿⣿
+        --     ⠁⠀⣴⣿⣿⣿⣿⠿⠿⣿⣿⣿⣿⣿⣦⡙⠻⣶⣄⡀⠀⠈⠙⢿⣿⣷⣦⣤⣤⣴⣿⡏⣠⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣌⠻⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⣿⣿⣿
+        --     ⠀⢸⣿⣿⣿⠋⣠⠔⠀⠀⠻⣿⣿⣿⣿⢉⡳⢦⣉⠛⢷⣤⣀⠀⠈⠙⠿⣿⣿⣿⣿⢸⣿⡄⠻⣿⣿⠟⡈⣿⣿⣿⣿⣿⢉⣿⣧⢹⣿⣿⣄⠀⠀⠀⠀⠀⠀⠀⢠⣿⣿⣿⣿⣿⣿
+        --     ⠀⢸⣿⣿⡇⠠⡇⠀⠀⠀⠀⣿⣿⣿⣿⢸⣿⣷⣤⣙⠢⢌⡛⠷⣤⣄⠀⠈⠙⠿⣿⣿⣿⣿⣷⣦⣴⣾⣿⣤⣙⣛⣛⣥⣾⣿⣿⡌⣿⣿⣿⣷⣤⣀⣀⣀⣠⣴⣿⣿⣿⣿⣿⣿⣿
+        --     ⠀⢸⣿⣿⣷⡀⠡⠀⠀⠀⣰⣿⣿⣿⣿⢸⣿⣿⣿⣿⣿⣦⣌⡓⠤⣙⣿⣦⡄⠀⠈⠙⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢡⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+        --     ⠀⢸⣿⣿⣿⣿⣶⣤⣴⣾⣿⣿⣿⣿⣿⢸⣿⣿⣿⣿⣿⣿⣿⣿⣷⣾⣿⣿⣷⠀⣶⡄⠀⠈⠙⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⢃⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+        --     ⠀⢸⣿⣿⣿⣿⣿⠟⠻⣿⣿⡏⣉⣭⣭⡘⠻⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⣿⡇⢸⡇⢠⡀⠈⠙⠋⠉⠉⠉⠉⠛⠫⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+        --     ⠀⢸⣿⣿⠛⣿⣿⣀⣀⣾⡿⢀⣿⣿⣿⢻⣷⣦⢈⡙⠻⢿⣿⣿⣿⣿⣿⣿⣿⠀⣿⡇⢸⡇⢸⣿⠀⣦⠀⠀⠶⣶⣦⣀⠀⠘⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+        --     ⠀⢸⣿⣿⣦⣈⡛⠿⠟⣋⣤⣾⣿⣿⣿⣸⣿⣿⢸⡇⢰⡆⢈⡙⠻⢿⣿⣿⣿⠀⢿⡇⢸⡇⢸⣿⢠⣿⡇⣿⡆⢈⡙⠻⠧⠀⢹⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+        --     ⠀⠀⣝⠛⢿⣿⣿⣿⣿⣿⣿⠟⣁⠀⠀⢈⠛⠿⢸⣇⢸⡇⢸⡇⣶⣦⣌⡙⠻⢄⡀⠁⠘⠇⠘⣿⢸⣿⡇⣿⡇⢸⡛⠷⣦⣄⠀⠹⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+                -- ]]
+                local logo = [[
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣏⠙⠛⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡻⣿⠹⣿⡟⣿⢻⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣏⣰⠉⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⣏⣙⣢⣽⣷⣿⣷⣿⣿⣟⡼⢹⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⡿⢻⡓⣄⢝⢿⣿⣿⣿⣿⣿⣿⣿⣿⣁⣾⣿⣿⣿⣿⣿⣿⣿⣿⡿⠿⠛⢙⠿⠿⠿⠿⢛⣠
+⣿⣿⣿⣿⣿⣿⣿⣿⢃⢎⡇⣿⡌⣦⣉⠻⣿⣿⣿⣿⣿⣉⣬⣿⡿⠿⠿⠟⢛⣩⣥⣤⣶⣶⣾⣦⢱⣶⣘⢛⣛⣫
+⣿⣿⣿⣿⣿⣿⣿⠇⣞⣾⡇⣿⡇⣿⣿⣷⣌⡙⢿⣿⣿⣿⡾⢿⣿⣿⣿⣿⣿⡏⣤⣨⣿⣿⣿⣧⣬⡙⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⡏⢸⣿⣿⣷⣼⣇⢸⣿⣿⣿⣿⣦⣌⠻⣿⡖⢻⢿⣿⣿⣿⣿⡇⠿⢻⣿⣿⣿⣿⣿⡇⢸⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⡇⢻⣿⣿⣿⣿⣿⠈⣿⣿⣿⣿⣿⣿⣷⡤⠙⠭⣾⣿⣿⣿⣿⣿⠧⣹⣿⣿⣿⣿⣿⠃⣼⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣷⡘⢿⣿⣿⣿⣿⠇⣿⣿⣿⣿⣿⠟⣋⣤⣶⣶⣌⠒⢶⣬⡛⢧⣿⡿⣛⣭⣭⣧⣤⣾⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣦⣙⣛⣛⣋⣴⣿⣿⣿⡿⣡⣾⣿⣿⣿⣿⣿⣷⣦⡙⢿⣆⢧⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟⣰⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣦⣍⠊⣿⣿⣿⡿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢰⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⣦⣙⠋⣰⡄⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⠟⠛⠻⣿⣿⣿⣿⣿⣿⣿⠈⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⢸⡿⢃⣤⣐⠻⢿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⠁⣼⢿⣆⠸⣿⣿⣿⣿⣿⠋⣷⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣄⣠⣾⣿⣿⣿⣦⣽⣿⣿⣿
+⣿⣿⣿⣿⣿⠀⣧⣈⢁⣴⣿⣿⣿⣿⠏⣰⣿⡏⣿⡿⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣆⠹⣿⣿⣿⣿⣿⣿⣉⣴⠿⡟⣠⣿⢁⣾⡿⠃⣿⡿⠉⣿⡏⢩⣟⢉⣇⣸⣿⡿⠟⠛⠛⠿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣶⣤⣉⣉⣛⠉⣉⣡⣤⣶⡟⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣦⠘⣵⣾⣿⣿⣿⣶⡄⠹⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⡿⠿⣻⣿⣿⣿⣿⠟⣡⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣀⣉⡉⢻⣿⣿⣿⡟⠀⣿
+⣿⣿⣿⣿⣿⠟⣋⣥⣶⣿⣿⣿⣯⠩⣥⣶⣶⠄⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣍⠻⠟⣃⣼⣿⣿⣿⡏⢸⣿
+⣿⣿⠟⣋⣴⣿⣿⣿⣿⣿⣿⣿⣿⣷⣶⣶⣶⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⢸⣿
+⠟⢁⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡅⢸⣿
+        ]]
+
+        -- log 前 x 个换行，后n个换行
+        logo = string.rep("\n", 1) .. logo .. "\n"
+
+        local opts = {
+          theme = "doom",
+          hide = {
+            -- this is taken care of by lualine
+            -- enabling this messes up the actual laststatus setting after loading a file
+            statusline = false,
+          },
+          config = {
+            header = vim.split(logo, "\n"),
+          -- stylua: ignore
+          center = {
+            { action = 'lua LazyVim.pick()()',                           desc = " Find File",       icon = " ", key = "f" },
+            { action = "ene | startinsert",                              desc = " New File",        icon = " ", key = "n" },
+            { action = 'lua LazyVim.pick("oldfiles")()',                 desc = " Recent Files",    icon = " ", key = "r" },
+            { action = 'lua LazyVim.pick("live_grep")()',                desc = " Find Text",       icon = " ", key = "g" },
+            { action = 'lua LazyVim.pick.config_files()()',              desc = " Config",          icon = " ", key = "c" },
+            { action = 'lua require("persistence").load()',              desc = " Restore Session", icon = " ", key = "s" },
+            { action = "LazyExtras",                                     desc = " Lazy Extras",     icon = " ", key = "x" },
+            { action = "Lazy",                                           desc = " Lazy",            icon = "󰒲 ", key = "l" },
+            { action = function() vim.api.nvim_input("<cmd>qa<cr>") end, desc = " Quit",            icon = " ", key = "q" },
+          },
+            footer = function()
+              local stats = require("lazy").stats()
+              local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
+              return { "⚡ Neovim loaded " .. stats.loaded .. "/" .. stats.count .. " plugins in " .. ms .. "ms" }
+            end,
+          },
+        }
+
+        -- 设置高亮颜色
+        vim.api.nvim_set_hl(0, "DashboardIcon", { fg = "#9caa9d" })
+        vim.api.nvim_set_hl(0, "DashboardDesc", { fg = "#85868b" })
+        vim.api.nvim_set_hl(0, "DashboardKey", { fg = "#6f7887" })
+
+        for _, button in ipairs(opts.config.center) do
+          -- 使用 'nvim_set_hl' 设置按钮的颜色
+          button.icon = vim.fn.expand("%#DashboardIcon#") .. button.icon
+          button.desc = vim.fn.expand("%#DashboardDesc#") .. button.desc
+          -- 调整描述长度
+          button.desc = button.desc .. string.rep(" ", 43 - #button.desc)
+          button.key = vim.fn.expand("%#DashboardKey#") .. button.key
+          button.key_format = "[ %s ]"
+        end
+
+        -- open dashboard after closing lazy
+        if vim.o.filetype == "lazy" then
+          vim.api.nvim_create_autocmd("WinClosed", {
+            pattern = tostring(vim.api.nvim_get_current_win()),
+            once = true,
+            callback = function()
+              vim.schedule(function()
+                vim.api.nvim_exec_autocmds("UIEnter", { group = "dashboard" })
+              end)
+            end,
+          })
+        end
+
+        return opts
+      end,
+    },
+  },
+
+  {
+    "akinsho/bufferline.nvim",
+    event = "VeryLazy",
+    opts = {
+      options = {
+
+        offsets = {
+          {
+            filetype = "neo-tree",
+            text = "", -- Set the text to empty
+            highlight = "Directory",
+            text_align = "left",
+          },
+        },
+      },
+    },
+  },
+}
