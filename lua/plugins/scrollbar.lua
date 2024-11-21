126851b835b400d3871c4c95f41b5a47665c247f
joreh_ih <pipdreamer@foxmail.com>
2024-11-21 13:25:20 +0800

    1. 添加buffer,find recent files相关快捷键 2. 添加telescope 查找最近文件的插件 3. scrollbar添加git信息

--- lua/plugins/scrollbar.lua
+++ lua/plugins/scrollbar.lua
@@ -1,8 +1,18 @@
 return {
   {
     "petertriho/nvim-scrollbar",
+    dependencies = {
+      -- "kevinhwang91/nvim-hlslens",
+      "lewis6991/gitsigns.nvim",
+    },
     config = function()
       require("scrollbar").setup({})
+      require("gitsigns").setup({})
+      require("scrollbar.handlers.gitsigns").setup()
+      -- require("scrollbar.handlers.search").setup({
+        -- override_lens = function() end,
+      -- })
+
     end,
   },
 }
