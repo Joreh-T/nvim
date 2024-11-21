8d226e4174a0e66d8da08f00b59ab7ebb8cb3c41
TaoQh <taoqiuhao@inhand.com.cn>
2024-11-21 13:25:20 +0800

    1. 添加buffer,find recent files相关快捷键 2. 添加telescope 查找最近文件的插件 3. scrollbar添加git信息

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -11,8 +11,23 @@ return {
   { map("n", "<leader>r", "<cmd>LspRestart<CR>", { noremap = true, silent = false, desc = "Restart LSP" }) },
   { map("n", "<CR>", "o<esc>", { noremap = true, silent = true }) },
 
-  {map("n", "<a-`>",      function() Snacks.terminal(nil, { cwd = LazyVim.root() }) end, { desc = "Terminal (Root Dir)" })},
-  {map("t", "<a-`>", "<cmd>close<cr>", { desc = "Hide Terminal" })},
+  {
+    map("n", "<a-`>", function()
+      Snacks.terminal(nil, { cwd = LazyVim.root() })
+    end, { desc = "Terminal (Root Dir)" }),
+  },
+
+  { map("n", "<leader>bh", "<cmd>bprevious<cr>", { desc = "Prev Buffer" }) },
+  { map("n", "<leader>bl", "<cmd>bnext<cr>", { desc = "Next Buffer" }) },
+  { map("n", "<leader>sb", "<cmd>Telescope current_buffer_fuzzy_find<cr>", { desc = "Search In Curren Buffer" }) },
+
+  {
+    map("n", "<leader>fr", function()
+      require("telescope").extensions["recent-files"].recent_files({})
+    end, { noremap = true, silent = true, desc = "Recent Files" }),
+  },
+
+  { map("t", "<a-`>", "<cmd>close<cr>", { desc = "Hide Terminal" }) },
   -----------------------------------------------------------
   { map({ "n", "v" }, "<S-h>", "^", { noremap = true, silent = true }) },
   { map({ "n", "v" }, "<S-l>", "$", { noremap = true, silent = true }) },
@@ -28,7 +43,7 @@ return {
       vim.diagnostic.open_float(nil, {
         border = "rounded", -- You can choose "single", "double", "rounded", or "solid"
       })
-    end, { noremap = true, silent = true, desc = "Hover diagnostic"}),
+    end, { noremap = true, silent = true, desc = "Hover diagnostic" }),
   },
   --   {map("n", "gk", "<cmd>lua vim.diagnostic.goto_prev()<CR>", { noremap = true, silent = true })},
   --   {map("n", "gj", "<cmd>lua vim.diagnostic.goto_next()<CR>", { noremap = true, silent = true })}
