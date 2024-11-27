72aba4a9c9463058557a3f008392c3a1c9e8e827
joreh_ih <pipdreamer@foxmail.com>
2024-11-27 16:39:41 +0800

    disable virtua text in diagnostics

--- lua/plugins/clangd.lua
+++ lua/plugins/clangd.lua
@@ -1,6 +1,10 @@
 return {
   {
     "neovim/nvim-lspconfig", -- 引入 nvim-lspconfig 插件
+    dependencies = {
+      "mason.nvim",
+      { "williamboman/mason-lspconfig.nvim", config = function() end },
+    },
     opts = {
       -- 在 opts 中配置 clangd
       servers = {
@@ -30,6 +34,27 @@ return {
           },
           on_attach = function(client, bufnr) end,
         },
+      },
+
+      -- 诊断配置
+      diagnostics = {
+        virtual_text = false,  -- 禁用虚拟文本
+      underline = true,
+      update_in_insert = false, -- 插入模式下不进行诊断
+      severity_sort = true, -- 诊断信息按严重性排序。通常，错误会排在最前面，警告排在其次，信息和提示排在最后。
+      --   signs = {
+      --     text = {
+      --       [vim.diagnostic.severity.ERROR] = LazyVim.config.icons.diagnostics.Error,
+      --       [vim.diagnostic.severity.WARN] = LazyVim.config.icons.diagnostics.Warn,
+      --       [vim.diagnostic.severity.HINT] = LazyVim.config.icons.diagnostics.Hint,
+      --       [vim.diagnostic.severity.INFO] = LazyVim.config.icons.diagnostics.Info,
+      --     },
+      --   },
+      },
+
+      -- 禁用 CodeLens
+      codelens = {
+        enabled = false,  -- 禁用 CodeLens
       },
     },
   },
