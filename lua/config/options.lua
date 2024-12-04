46191067fb6154811dc710c168e1f181e71d1b7c
pipdreamer <pipdreamer@qq.com>
2024-12-04 22:09:54 +0800

    replace autocmds with my plugin placeholder_highlight

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -22,6 +22,7 @@ vim.opt.list = true -- 显示不可见字符
 vim.opt.tabstop = 4 -- 设置 tab 键的宽度为 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4 -- 设置自动缩进时的宽度为 4
+vim.opt.copyindent = true
 vim.opt.expandtab = true -- 插入tab 转换为空格
 vim.opt.listchars = { space = "·", tab = ">-", eol = "↲"}
 vim.opt.smartindent = true
