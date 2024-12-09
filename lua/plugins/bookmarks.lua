9de4fa6d4bcb5af6185030073362c498b4b6da1d
joreh_ihw <pipdreamer@foxmail.com>
2024-12-09 11:11:40 +0800

    设置启动时就加载bookmark，不需要等到<tab><tab>

--- lua/plugins/bookmarks.lua
+++ lua/plugins/bookmarks.lua
@@ -1,12 +1,28 @@
 return {
   "crusj/bookmarks.nvim",
-  keys = {
-    { "<tab><tab>", mode = { "n" } },
-  },
+  -- keys = {
+  --   { "<tab><tab>", mode = { "n" } },
+  -- },
   branch = "main",
   dependencies = { "kyazdani42/nvim-web-devicons" },
   config = function()
     require("bookmarks").setup({
+
+      -- default keymap
+      -- keymap = {
+      --   toggle = "<tab><tab>", -- Toggle bookmarks(global keymap)
+      --   close = "q", -- close bookmarks (buf keymap)
+      --   add = "\\z", -- Add bookmarks(global keymap)
+      --   add_global = "\\g", -- Add global bookmarks(global keymap), global bookmarks will appear in all projects. Identified with the symbol '󰯾'
+      --   jump = "<CR>", -- Jump from bookmarks(buf keymap)
+      --   delete = "dd", -- Delete bookmarks(buf keymap)
+      --   order = "<space><space>", -- Order bookmarks by frequency or updated_time(buf keymap)
+      --   delete_on_virt = "\\dd", -- Delete bookmark at virt text line(global keymap)
+      --   show_desc = "\\sd", -- show bookmark desc(global keymap)
+      --   focus_tags = "<c-j>", -- focus tags window
+      --   focus_bookmarks = "<c-k>", -- focus bookmarks window
+      --   toogle_focus = "<S-Tab>", -- toggle window focus (tags-window <-> bookmarks-window)
+      -- },
       width = 0.8, -- Bookmarks window width:  (0, 1]
       height = 0.7, -- Bookmarks window height: (0, 1]
       preview_ratio = 0.42, -- Bookmarks preview window ratio (0, 1]
@@ -15,7 +31,22 @@ return {
 
       virt_text = "", -- Show virt text at the end of bookmarked lines, if it is empty, use the description of bookmarks instead.
       sign_icon = "📑", -- if it is not empty, show icon in signColumn.
-      virt_pattern = { "*.go", "*.lua", "*.sh", "*.php", "*.rs", "*.c", "*.cpp", "*.h", "*.hpp", "*.py", "*.xml", "*.yaml", "*.xml", "*.html"}, -- Show virt text only on matched pattern
+      virt_pattern = {
+        "*.go",
+        "*.lua",
+        "*.sh",
+        "*.php",
+        "*.rs",
+        "*.c",
+        "*.cpp",
+        "*.h",
+        "*.hpp",
+        "*.py",
+        "*.xml",
+        "*.yaml",
+        "*.xml",
+        "*.html",
+      }, -- Show virt text only on matched pattern
       virt_ignore_pattern = {}, -- Ignore showing virt text on matched pattern, this works after virt_pattern
       border_style = "rounded", -- 可选："single", "double", "rounded"
       hl = {
