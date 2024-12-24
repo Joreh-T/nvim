33958104696a0583595975197856226279b3e6e2
pipdreamer <pipdreamer@qq.com>
2024-12-24 23:38:57 +0800

    1. 修复cmp.lua中错误的providers配置（顺便关了）；2. 启用搜索高亮

--- lua/plugins/scrollbar.lua
+++ lua/plugins/scrollbar.lua
@@ -2,16 +2,16 @@ return {
   {
     "petertriho/nvim-scrollbar",
     dependencies = {
-      -- "kevinhwang91/nvim-hlslens",
+      "kevinhwang91/nvim-hlslens",
       "lewis6991/gitsigns.nvim",
     },
     config = function()
       require("scrollbar").setup({})
       require("gitsigns").setup({})
       require("scrollbar.handlers.gitsigns").setup()
-      -- require("scrollbar.handlers.search").setup({
+      require("scrollbar.handlers.search").setup({
         -- override_lens = function() end,
-      -- })
+      })
 
     end,
   },
