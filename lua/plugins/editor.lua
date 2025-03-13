c27e1b3aee2f6a1803963317b33a975b7867845c
pipdreamer <pipdreamer@qq.com>
2025-03-13 23:41:34 +0800

    添加鼠标hover显示lsp信息插件

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -35,7 +35,7 @@ return {
     opts = function(_, opts)
       opts.winopts = {
         width = 0.8,
-        height = 0.8,
+        height = 0.85,
         row = 0.5,
         col = 0.5,
         preview = {
@@ -127,11 +127,49 @@ return {
   },
   {
     -- 在editor中显示16进制颜色
-    'norcalli/nvim-colorizer.lua',
+    "norcalli/nvim-colorizer.lua",
     config = function()
-    require("colorizer").setup()
-    end
-  }
+      require("colorizer").setup()
+    end,
+  },
+  {
+    "lewis6991/hover.nvim",
+    config = function()
+      require("hover").setup({
+        init = function()
+          -- Require providers
+          require("hover.providers.lsp")
+          -- require('hover.providers.gh')
+          -- require('hover.providers.gh_user')
+          -- require('hover.providers.jira')
+          -- require('hover.providers.dap')
+          -- require('hover.providers.fold_preview')
+          -- require('hover.providers.diagnostic')
+          -- require('hover.providers.man')
+          -- require('hover.providers.dictionary')
+        end,
+        preview_opts = {
+          border = "none",
+        },
+        -- Whether the contents of a currently open hover window should be moved
+        -- to a :h preview-window when pressing the hover keymap.
+        preview_window = false,
+        title = false,
+        mouse_providers = {
+          "LSP",
+        },
+        mouse_delay = 1000,
+      })
 
-
+      -- Setup keymaps
+      -- vim.keymap.set("n", "K", require("hover").hover, {desc = "hover.nvim"})
+      -- vim.keymap.set("n", "gK", require("hover").hover_select, {desc = "hover.nvim (select)"})
+      -- vim.keymap.set("n", "<C-p>", function() require("hover").hover_switch("previous") end, {desc = "hover.nvim (previous source)"})
+      -- vim.keymap.set("n", "<C-n>", function() require("hover").hover_switch("next") end, {desc = "hover.nvim (next source)"})
+      --
+      -- Mouse support
+      vim.keymap.set("n", "<MouseMove>", require("hover").hover_mouse, { desc = "hover.nvim (mouse)" })
+      vim.o.mousemoveevent = true
+    end,
+  },
 }
