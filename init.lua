5f0e6f8540b512502541348a90dfba4d564cb9f0
joreh_ihw <pipdreamer@foxmail.com>
2025-04-09 18:13:56 +0800

    添加neovide配置

--- init.lua
+++ init.lua
@@ -1,2 +1,9 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+local icons = require("lazyvim.config").icons
+
+icons.diagnostics.Error = " "
+icons.diagnostics.Warn = " "
+icons.diagnostics.Info = " "
+icons.diagnostics.Hint = "󰌵 "
