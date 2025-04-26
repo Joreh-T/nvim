122d46b22992126d50a3ba3bad2a8130e8943d5a
pipdreamer <pipdreamer@foxmail.com>
2025-04-26 20:32:04 +0800

    优化UI

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -3,31 +3,37 @@ return {
     optional = true,
     event = "VeryLazy",
     opts = function(_, opts)
-        -- 直接覆盖 lualine_c 配置
-      local icons = LazyVim.config.icons
+        -- +-------------------------------------------------+
+        -- | A | B | C                             X | Y | Z |
+        -- +-------------------------------------------------+
+        local icons = LazyVim.config.icons
+        opts.sections.lualine_a = { "mode" }
+        opts.sections.lualine_b = { "branch" }
         opts.sections.lualine_c = {
             LazyVim.lualine.root_dir(),
             {
-              "diagnostics",
-              symbols = {
-                error = icons.diagnostics.Error,
-                warn = icons.diagnostics.Warn,
-                info = icons.diagnostics.Info,
-                hint = icons.diagnostics.Hint,
-              },
+                "diagnostics",
+                symbols = {
+                    error = icons.diagnostics.Error,
+                    warn = icons.diagnostics.Warn,
+                    info = icons.diagnostics.Info,
+                    hint = icons.diagnostics.Hint,
+                },
             },
             { "filetype", icon_only = true, separator = "", padding = { left = 1, right = 0 } },
-            { LazyVim.lualine.pretty_path(), separator = "" },  -- 去除路径后的分隔符
+            { LazyVim.lualine.pretty_path(), separator = "" }, -- 去除路径后的分隔符
             {
                 function()
-                    return vim.bo.modified and "[+]" or "[-]"
+                    return vim.bo.modified and "●" or " "
                 end,
                 color = function()
-                    return vim.bo.modified and { fg = "#e5c07b" } or { fg = "#98c379" }
+                    -- return vim.bo.modified and { fg = "#e5c07b" } or { fg = "#98c379" }
+                    local group = vim.bo.modified and "NeoTreeGitModified" or "Green"
+                    local hl = vim.api.nvim_get_hl(0, { name = group })
+                    return { fg = hl.fg and string.format("#%06x", hl.fg) or "#e5c07b" }
                 end,
                 padding = { left = 0, right = 1 },
-            }
-
+            },
 
             -- {
             --     function() return vim.bo.modified and "[+]" or "[-]" end,
@@ -35,25 +41,73 @@ return {
             --     padding = { left = 0, right = 1 },
             -- },
         }
+        opts.sections.lualine_x = {
+            Snacks.profiler.status(),
+            -- stylua: ignore
+            {
+              function() return require("noice").api.status.command.get() end,
+              cond = function() return package.loaded["noice"] and require("noice").api.status.command.has() end,
+              color = function() return { fg = Snacks.util.color("Statement") } end,
+            },
+            -- stylua: ignore
+            {
+              function() return require("noice").api.status.mode.get() end,
+              cond = function() return package.loaded["noice"] and require("noice").api.status.mode.has() end,
+              color = function() return { fg = Snacks.util.color("Constant") } end,
+            },
+            -- stylua: ignore
+            {
+              function() return "  " .. require("dap").status() end,
+              cond = function() return package.loaded["dap"] and require("dap").status() ~= "" end,
+              color = function() return { fg = Snacks.util.color("Debug") } end,
+            },
+            -- stylua: ignore
+            -- {
+            --   require("lazy.status").updates,
+            --   cond = require("lazy.status").has_updates,
+            --   color = function() return { fg = Snacks.util.color("Special") } end,
+            -- },
+            {
+                "diff",
+                symbols = {
+                    added = icons.git.added,
+                    modified = icons.git.modified,
+                    removed = icons.git.removed,
+                },
+                source = function()
+                    local gitsigns = vim.b.gitsigns_status_dict
+                    if gitsigns then
+                        return {
+                            added = gitsigns.added,
+                            modified = gitsigns.changed,
+                            removed = gitsigns.removed,
+                        }
+                    end
+                end,
+            },
+        }
+        opts.sections.lualine_y = {
+            { "progress", separator = " ", padding = { left = 1, right = 0 } },
+            { "location", padding = { left = 0, right = 1 } },
+        }
         opts.sections.lualine_z = {}
 
         if vim.g.trouble_lualine and LazyVim.has("trouble.nvim") then
-        local trouble = require("trouble")
-        local symbols = trouble.statusline({
-          mode = "symbols",
-          groups = {},
-          title = false,
-          filter = { range = true },
-          format = "{kind_icon}{symbol.name:Normal}",
-          hl_group = "lualine_c_normal",
-        })
-        table.insert(opts.sections.lualine_c, {
-          symbols and symbols.get,
-          cond = function()
-            return vim.b.trouble_lualine ~= false and symbols.has()
-          end,
-        })
-      end
+            local trouble = require("trouble")
+            local symbols = trouble.statusline({
+                mode = "symbols",
+                groups = {},
+                title = false,
+                filter = { range = true },
+                format = "{kind_icon}{symbol.name:Normal}",
+                hl_group = "lualine_c_normal",
+            })
+            table.insert(opts.sections.lualine_c, {
+                symbols and symbols.get,
+                cond = function()
+                    return vim.b.trouble_lualine ~= false and symbols.has()
+                end,
+            })
+        end
     end,
-
 }
