6496a9b4d9a2cd69a22704dad95332c5c358a748
pipdreamer <pipdreamer@foxmail.com>
2025-05-07 23:35:00 +0800

    1. 优化nvim版本比较接口；2. 修改gitsigns.nvim的版本安装条件；添加lazygit.nvim插件（可以在nvim中对lazygit进行配置）

--- lua/plugins/scrollbar.lua
+++ lua/plugins/scrollbar.lua
@@ -6,7 +6,7 @@ return {
         "kevinhwang91/nvim-hlslens",
         {
             "lewis6991/gitsigns.nvim",
-            commit = utils.is_nvim_ge(0, 10) and "ee7e50dfbdf49e3acfa416fd3ad3abbdb658582c" or nil,
+            commit = utils.is_nvim_le(0, 10) and "ee7e50dfbdf49e3acfa416fd3ad3abbdb658582c" or nil,
         },
     },
     config = function()
